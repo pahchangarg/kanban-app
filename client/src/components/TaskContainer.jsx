d1f9940b028a96e424442080982fe2ec6a90d06e
pahchangarg <72337426+pahchangarg@users.noreply.github.com>
2023-11-23 13:07:41 +0530

    Connected server and client
    Connected server with client and used cors for the origin. Used react beautiful dnd for drop objects

--- client/src/components/TaskContainer.jsx
+++ client/src/components/TaskContainer.jsx
@@ -1,37 +1,60 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const TaskContainer = (props, { socket }) => {
+  const [tasks, setTasks] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await fetch("http://localhost:5001/api");
+        const json = await data.json();
+        setTasks(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, []);
+
+  function handleDragEnd() {
+    return;
+  }
+
   return (
     <div className="container">
-      <div className="pending__wrapper">
-        <div className="pending__items">
-          <p>Debug</p>
-          <p className="comment">
-            <Link to="/comments">2 comments</Link>
-          </p>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {Object.entries(tasks).map((task) => (
+          <div className="pending__wrapper">
+            <div className="pending__items">
+              <p>Debug</p>
+              <p className="comment">
+                <Link to="/comments">2 comments</Link>
+              </p>
+              {props.children}
+            </div>
+          </div>
+        ))}
+        <div className="ongoing__wrapper">
+          <div className="ongoing__items">
+            <p>Ongoing</p>
+            <p className="comment">
+              <Link to="/comments">2 comments</Link>
+            </p>
+            {props.children}
+          </div>
         </div>
-        {props.children}
-      </div>
-      <div className="ongoing__wrapper">
-        <div className="ongoing__items">
-          <p>Ongoing</p>
-          <p className="comment">
-            <Link to="/comments">2 comments</Link>
-          </p>
+        <div className="completed__wrapper">
+          <div className="completed__items">
+            <p>Completed</p>
+            <p className="comment">
+              <Link to="/comments">2 comments</Link>
+            </p>
+            {props.children}
+          </div>
         </div>
-        {props.children}
-      </div>
-
-      <div className="completed__wrapper">
-        <div className="completed__items">
-          <p>Completed</p>
-          <p className="comment">
-            <Link to="/comments">2 comments</Link>
-          </p>
-        </div>
-        {props.children}
-      </div>
+      </DragDropContext>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -3,18 +3,70 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const cors = require("cors");
 
-const io = new Server(server);
+app.use(cors());
+const UID = () => Math.random().toString(36).substring(2, 10);
+const tasks = {
+  pending: {
+    title: "pending",
+    items: [
+      {
+        id: UID(),
+        title: "Send the Figma file to Dima",
+        comments: [],
+      },
+    ],
+  },
+  ongoing: {
+    title: "ongoing",
+    items: [
+      {
+        id: UID(),
+        title: "Review GitHub issues",
+        comments: [
+          {
+            name: "David",
+            text: "Ensure you review before merging ",
+            id: UID(),
+          },
+        ],
+      },
+    ],
+  },
+  completed: {
+    title: "completed",
+    items: [
+      {
+        id: UID(),
+        title: "Create technical contents",
+        comments: [
+          {
+            name: "Dima",
+            text: "Make sure you check the requirements",
+            id: UID(),
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const socketIO = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 const PORT = 5001;
 
-app.get("/", (req, res) => {
-  res.send("Hello sujal!!");
+app.get("/api", (req, res) => {
+  res.json(tasks);
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+socketIO.on("connection", (socket) => {
+  console.log(`${socket.id} user connected`);
 });
 
 server.listen(PORT, () => {
-  console.log("server is running on ${PORT}");
+  console.log(`server is running on ${PORT}`);
 });
